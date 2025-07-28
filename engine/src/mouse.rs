2ac12dd832ab4af4ef6023e2c095f4efc23da4b6
t_ueda <uetatsu1005@gmail.com>
2025-07-29 01:28:53 +0900

    Resolved dangling pointer by mistake

--- engine/src/mouse.rs
+++ engine/src/mouse.rs
@@ -1,31 +1,27 @@
 // TODO: Consider using GetCursorInfo, GetCursorPos, GetCursor, & GetPhysicalCursorPos
 
 #[unsafe(no_mangle)]
-pub extern "stdcall" fn mmv(dx: i32, dy: i32) {
+pub extern "stdcall" fn mmv(dx: i32, dy: i32) -> i32 {
     use winapi::um::winuser::{INPUT, MOUSEINPUT, SendInput};
 
-    let cinputs = 1; // Number of inputs to send
-    let pinputs = {
-        let mouse_input = MOUSEINPUT {
-            dx,
-            dy,
-            mouseData: 0x0,
-            dwFlags: 0x0001, // MOUSEEVENTF_MOVE
-            time: 0x0,
-            dwExtraInfo: 0x0,
-        };
-        let mut input = INPUT {
-            type_: 0,                                       // INPUT_MOUSE
-            u: unsafe { std::mem::transmute(mouse_input) }, // union
-        };
-
-        &mut input as *mut INPUT
+    let mouse_input = MOUSEINPUT {
+        dx,
+        dy,
+        mouseData: 0x0,
+        dwFlags: 0x0001, // MOUSEEVENTF_MOVE
+        time: 0x0,
+        dwExtraInfo: 0x0,
     };
+    let mut input = INPUT {
+        type_: 0,                                       // INPUT_MOUSE
+        u: unsafe { std::mem::transmute(mouse_input) }, // union
+    };
+
+    let cinputs = 1; // Number of inputs to send
+    let pinputs = &mut input as *mut INPUT;
     let cbsize = std::mem::size_of::<INPUT>();
 
-    unsafe {
-        SendInput(cinputs, pinputs, cbsize as i32);
-    }
+    unsafe { SendInput(cinputs, pinputs, cbsize as i32) as i32 }
 }
 
 // FIXME: Below code is not tested yet, needs to be verified
