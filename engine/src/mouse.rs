1ca6efb31ac4cd7f3b78dd3ec4807e4b7479594d
t_ueda <uetatsu1005@gmail.com>
2025-07-28 23:55:42 +0900

    Refactor

--- engine/src/mouse.rs
+++ engine/src/mouse.rs
@@ -2,30 +2,29 @@
 
 #[unsafe(no_mangle)]
 pub extern "stdcall" fn mmv(dx: i32, dy: i32) {
-    use winapi::um::winuser::{INPUT, LPINPUT, MOUSEINPUT, SendInput};
-
-    let mouse_input = MOUSEINPUT {
-        dx,
-        dy,
-        mouseData: 0x0,
-        dwFlags: 0x0001, // MOUSEEVENTF_MOVE
-        time: 0x0,
-        dwExtraInfo: 0x0,
-    };
-
-    let mut input: INPUT = INPUT {
-        type_: 0,                                       // INPUT_MOUSE
-        u: unsafe { std::mem::transmute(mouse_input) }, // union
-    };
+    use winapi::um::winuser::{INPUT, MOUSEINPUT, SendInput};
 
     let cinputs = 1; // Number of inputs to send
+    let pinputs = {
+        let mouse_input = MOUSEINPUT {
+            dx,
+            dy,
+            mouseData: 0x0,
+            dwFlags: 0x0001, // MOUSEEVENTF_MOVE
+            time: 0x0,
+            dwExtraInfo: 0x0,
+        };
+        let mut input = INPUT {
+            type_: 0,                                       // INPUT_MOUSE
+            u: unsafe { std::mem::transmute(mouse_input) }, // union
+        };
 
-    let pinputs: LPINPUT = &mut input as *mut INPUT; // Cast to raw pointer
-
-    let cbsize = std::mem::size_of::<INPUT>() as i32;
+        &mut input as *mut INPUT
+    };
+    let cbsize = std::mem::size_of::<INPUT>();
 
     unsafe {
-        SendInput(cinputs, pinputs, cbsize);
+        SendInput(cinputs, pinputs, cbsize as i32);
     }
 }
 
