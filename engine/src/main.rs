5b051f07e6bdd42a72296120ef7b9a0587531717
t_ueda <uetatsu1005@gmail.com>
2025-07-29 21:42:16 +0900

    Update main.rs

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,6 +1,23 @@
 mod mouse;
+mod screen;
+mod structs;
 
 fn main() {
-    use mouse::mmv;
-    mmv(100, 100);
+    {
+        mouse::mov_abs_scp(555, 555);
+        let r = mouse::get_cur_pos();
+        println!("Mouse position should be (555, 555): ({}, {})", r.x, r.y);
+    };
+    {
+        mouse::mov_abs_si(555, 555);
+        let r = mouse::get_cur_pos();
+        println!("Mouse position should be (555, 555): ({}, {})", r.x, r.y);
+    };
+    {
+        // Mouse speed affects the movement
+        mouse::mov_abs_scp(0, 0);
+        mouse::mov_rel(100, 100);
+        let r = mouse::get_cur_pos();
+        println!("Mouse position: ({}, {})", r.x, r.y);
+    };
 }
